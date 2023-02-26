ce9e0d3f24d23b00620673de84b4a36812faaa79
zabih.s <67234564+zabih65@users.noreply.github.com>
2023-02-27 00:57:49 +0330

    update

---
+++ pages/blog.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const Blog = () => {
+    return (
+        <>
+
+            <h1>
+                salam bar zabih
+            </h1>
+            <Link href="/">
+                return to home page
+            </Link>
+        </>
+    );
+}
+
+export default Blog;
